d740f3d254dd721640381079e49937342c42503b
KwonHalim <129161091+KwonHalim@users.noreply.github.com>
2025-04-07 14:03:28 +0900

    :sparkles: 일반 로그인 구현

---
+++ user/src/main/java/befly/user/dto/SignUpRequest.java
@@ -0,0 +1,14 @@
+package befly.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpRequest {
+    private String userName;
+    private String email;
+    private String password;
+    private String nickname;
+    private String photoUrl; // 사진 URL 추가
+}
---
+++ user/src/main/java/befly/user/service/SignUpService.java
@@ -0,0 +1,68 @@
+package befly.user.service;
+
+import befly.common.code.status.GlobalErrorStatus;
+import befly.common.exception.RestApiException;
+import befly.user.domain.User;
+import befly.user.dto.SignUpRequest;
+import befly.user.repository.UserRepository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@Slf4j
+@RequiredArgsConstructor
+public class SignUpService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 회원가입 로직
+     * @param signUpRequest 유저 정보 담겨있음
+     * @return user: 저장된 유저 객체
+     */
+    public User signUp(SignUpRequest signUpRequest) {
+        log.info("SignUp request started for userName: {}", signUpRequest.getUserName());
+//       check email, nickname duplicate
+        checkForDuplicates(signUpRequest);
+//        PasswordEncoding
+        String encodedPassword = encodePassword(signUpRequest.getPassword());
+//        save user
+        User user = saveUser(signUpRequest, encodedPassword);
+
+        log.info("SignUp completed for userName: {} with userId: {}", user.getUserName(), user.getUserId());
+        return user;
+
+    }
+
+
+    private void checkForDuplicates(SignUpRequest signUpRequest) {
+        if (userRepository.existsByNickname(signUpRequest.getNickname())) {
+            throw new RestApiException(GlobalErrorStatus.DUPLICATE_NICKNAME);
+        }
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new RestApiException(GlobalErrorStatus.DUPLICATE_EMAIL);
+        }
+    }
+
+
+    //HACK URL 이미지 경로 하드코딩되어있음
+    private User saveUser(SignUpRequest signUpRequest, String encodedPassword) {
+        return userRepository.save(User.builder()
+                .userName(signUpRequest.getUserName())
+                .nickname(signUpRequest.getNickname())
+                .email(signUpRequest.getEmail())
+                .password(encodedPassword)
+                .wing(0L)
+                .badge(0L)
+                .profileImg("test.url")
+                .build());
+    }
+
+    String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+}
