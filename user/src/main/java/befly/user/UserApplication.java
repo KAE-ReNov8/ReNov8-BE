a98dd8c99dff00c4066617bc80804aa141641f4d
KwonHalim <129161091+KwonHalim@users.noreply.github.com>
2025-04-08 09:03:46 +0900

    :bug: fix: [KAN-100] 스캔 패키지 추가

--- user/src/main/java/befly/user/UserApplication.java
+++ user/src/main/java/befly/user/UserApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication()
+@SpringBootApplication(scanBasePackages = {"befly.user", "befly.common"})
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
